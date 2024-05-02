32570f1d42dc10e386813ecfa46e162131b9dd40
icaraga <slqughterhquse@gmail.com>
2024-05-02 10:07:33 -0700

    created JS and linked it to all pages

---
+++ script.js
@@ -0,0 +1,17 @@
+var carouselImg = {
+    allegories: [],
+    bruxist: [],
+    myopic: [],
+    slowSaga: [],
+    budg: [],
+    juriExpi: [],
+}
+
+var carouselDesc = {
+    allegories: [],
+    bruxist: [],
+    myopic: [],
+    slowSaga: [],
+    budg: [],
+    juriExpi: [],
+}

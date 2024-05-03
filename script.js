4431a0e39c0627f0a6b83afd495b8444f0ea8ebd
icaraga <slqughterhquse@gmail.com>
2024-05-03 10:36:23 -0700

    added all image content, created portfolio landing page

--- script.js
+++ script.js
@@ -2,16 +2,10 @@ var carouselImg = {
     allegories: [],
     bruxist: [],
     myopic: [],
-    slowSaga: [],
-    budg: [],
-    juriExpi: [],
 }
 
 var carouselDesc = {
     allegories: [],
     bruxist: [],
     myopic: [],
-    slowSaga: [],
-    budg: [],
-    juriExpi: [],
 }
